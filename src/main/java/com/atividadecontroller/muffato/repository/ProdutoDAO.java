982124aece1bad17a287fcd088e1b44952d501b9
Felipe1632 <felipesouzaribeiro24@gmail.com>
2026-04-17 17:29:14 -0300

    atv

---
+++ src/main/java/com/atividadecontroller/muffato/repository/ProdutoDAO.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.atividadecontroller.muffato.repository;
+
+import com.atividadecontroller.muffato.model.ProdutoBean;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Aluno
+ */
+public class ProdutoDAO {
+}
