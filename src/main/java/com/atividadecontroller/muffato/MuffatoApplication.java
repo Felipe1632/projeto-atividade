4c21337e069e0298908a1341bcd18e2b3db182f6
Felipe1632 <felipesouzaribeiro24@gmail.com>
2026-04-17 16:54:49 -0300

    init commit

---
+++ src/main/java/com/atividadecontroller/muffato/MuffatoApplication.java
@@ -0,0 +1,13 @@
+package com.atividadecontroller.muffato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuffatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuffatoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atividadecontroller/muffato/MuffatoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atividadecontroller.muffato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuffatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
